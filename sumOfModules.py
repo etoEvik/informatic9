7831d6d50ebfeed49c5d023df88552a4f1926ea4
t___evik___ <eva@ptic.in.ua>
2023-11-15 23:42:53 +0200

    init

--- sumOfModules.py
+++ sumOfModules.py
@@ -15,4 +15,12 @@ totalSum = addNumbers(maxNumbers)
 print('Сума модулів введених чисел: ' + str(totalSum))
 
 
+# a1 = float(input("Enter a1: "))
+# a2 = float(input("Enter a2: "))
+# a3 = float(input("Enter a3: "))
+# a4 = float(input("Enter a4: "))
+# a5 = float(input("Enter a5: "))
+
+# result = abs(a1) + abs(a2) + abs(a3) + abs(a4) + abs(a5)
+# print("The absolute sum is", result)
 
